9a68ddb0d06c00d2bf4d2df28c4d540d8afb851d
jenspatel237 <Jenspatel237@users.noreply.github.com>
2019-10-07 19:18:50 -0400

    Create PktFilterDemo.java

---
+++ Project/src/project/PktFilterDemo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import net.sourceforge.jpcap.capture.CaptureTest;
+
+
+/**
+ *
+ * @author jens
+ */
+
+public class PktFilterDemo {
+
+    
+    public static void main(String[] args) 
+    {
+        CaptureTest ct = new CaptureTest();
+    }
+    
+}
